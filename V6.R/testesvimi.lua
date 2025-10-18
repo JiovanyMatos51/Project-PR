7415c101fe399f49b799224da52e762c6d33f274
JiovanyMatos51 <jiovanymatos@gmail.com>
2025-10-18 21:48:09 +0000

    Update version and enhance GUI functionality
    Updated version to v1.5.0 and modified GUI elements including buttons and their functionalities. Added teleportation and speed adjustment features.

--- V6.R/testesvimi.lua
+++ V6.R/testesvimi.lua
@@ -1,12 +1,14 @@
 -- Information
 local title = "Kofu SAB Hacks"
-local versao = "v1.4.0"
+local versao = "v1.5.0"
 local logotitle = "KF"
 local canvasy = 1.5
 
 -- Variables
 local plr = game.Players.LocalPlayer
+local Character = plr.Character or plr.CharacterAdded:Wait()
 local plrgui = plr:WaitForChild("PlayerGui")
+local Camera = workspace.CurrentCamera
 
 -- Tabelas
 local Services = {
@@ -28,18 +30,6 @@ local IgnoreList = {
 	"UIGradient",
 	"UIStroke",
 	"UIListLayout"
-}
-
-local NoClipList = {
-	"structure base home",
-	"Main"
-}
-
-local NoClipModelsList = {
-	"Side 1",
-	"Side 2",
-	"Side 3",
-	"LaserHitbox",
 }
 
 -- Create GUI
@@ -106,155 +96,155 @@ local UIGridLayout = Instance.new("UIGridLayout")
 UIGridLayout.FillDirection = Enum.FillDirection.Vertical
 UIGridLayout.HorizontalAlignment = Enum.HorizontalAlignment.Center
 UIGridLayout.VerticalAlignment = Enum.VerticalAlignment.Center
-UIGridLayout.CellSize = UDim2.new(0.83, 0, 0.1, 0)
+UIGridLayout.CellSize = UDim2.new(0.9, 0, 0.1, 0)
 UIGridLayout.CellPadding = UDim2.new(0, 0, 0, 20)
 UIGridLayout.Parent = ScrollingFrame
 
-local BTeleportBase = Instance.new("TextButton")
-BTeleportBase.Name = "BTeleportBase"
-BTeleportBase.Size = UDim2.new(0, 200, 0, 50)
-BTeleportBase.BorderColor3 = Color3.fromRGB(255, 255, 255)
-BTeleportBase.BackgroundTransparency = 0.1
-BTeleportBase.BorderSizePixel = 0
-BTeleportBase.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
-BTeleportBase.FontSize = Enum.FontSize.Size14
-BTeleportBase.TextStrokeTransparency = 0
-BTeleportBase.TextSize = 14
-BTeleportBase.TextColor3 = Color3.fromRGB(255, 255, 255)
-BTeleportBase.Text = "Fast TP"
-BTeleportBase.TextWrapped = true
-BTeleportBase.TextWrap = true
-BTeleportBase.Font = Enum.Font.FredokaOne
-BTeleportBase.TextScaled = true
-BTeleportBase.Parent = ScrollingFrame
+local ADash = Instance.new("TextButton")
+ADash.Name = "ADash"
+ADash.Size = UDim2.new(0, 200, 0, 50)
+ADash.BorderColor3 = Color3.fromRGB(255, 255, 255)
+ADash.BackgroundTransparency = 0.1
+ADash.BorderSizePixel = 0
+ADash.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
+ADash.FontSize = Enum.FontSize.Size14
+ADash.TextStrokeTransparency = 0
+ADash.TextSize = 14
+ADash.TextColor3 = Color3.fromRGB(255, 255, 255)
+ADash.Text = "Atravessar"
+ADash.TextWrapped = true
+ADash.TextWrap = true
+ADash.Font = Enum.Font.FredokaOne
+ADash.TextScaled = true
+ADash.Parent = ScrollingFrame
 
 local UICorner2 = Instance.new("UICorner")
 UICorner2.CornerRadius = UDim.new(0, 12)
-UICorner2.Parent = BTeleportBase
+UICorner2.Parent = ADash
 
 local UIStroke1 = Instance.new("UIStroke")
 UIStroke1.Color = Color3.fromRGB(255, 255, 255)
 UIStroke1.ApplyStrokeMode = Enum.ApplyStrokeMode.Border
 UIStroke1.Thickness = 2
-UIStroke1.Parent = BTeleportBase
+UIStroke1.Parent = ADash
 
-local CNoClip = Instance.new("TextButton")
-CNoClip.Name = "CNoClip"
-CNoClip.Size = UDim2.new(0, 200, 0, 50)
-CNoClip.BorderColor3 = Color3.fromRGB(255, 255, 255)
-CNoClip.BackgroundTransparency = 0.1
-CNoClip.BorderSizePixel = 0
-CNoClip.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
-CNoClip.FontSize = Enum.FontSize.Size14
-CNoClip.TextStrokeTransparency = 0
-CNoClip.TextSize = 14
-CNoClip.TextColor3 = Color3.fromRGB(255, 255, 255)
-CNoClip.Text = "No Clip"
-CNoClip.TextWrapped = true
-CNoClip.TextWrap = true
-CNoClip.Font = Enum.Font.FredokaOne
-CNoClip.TextScaled = true
-CNoClip.Parent = ScrollingFrame
+local ESalto = Instance.new("TextButton")
+ESalto.Name = "ESalto"
+ESalto.Size = UDim2.new(0, 200, 0, 50)
+ESalto.BorderColor3 = Color3.fromRGB(255, 255, 255)
+ESalto.BackgroundTransparency = 0.1
+ESalto.BorderSizePixel = 0
+ESalto.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
+ESalto.FontSize = Enum.FontSize.Size14
+ESalto.TextStrokeTransparency = 0
+ESalto.TextSize = 14
+ESalto.TextColor3 = Color3.fromRGB(255, 255, 255)
+ESalto.Text = "Salto"
+ESalto.TextWrapped = true
+ESalto.TextWrap = true
+ESalto.Font = Enum.Font.FredokaOne
+ESalto.TextScaled = true
+ESalto.Parent = ScrollingFrame
 
 local UICorner3 = Instance.new("UICorner")
 UICorner3.CornerRadius = UDim.new(0, 12)
-UICorner3.Parent = CNoClip
+UICorner3.Parent = ESalto
 
 local UIStroke2 = Instance.new("UIStroke")
 UIStroke2.Color = Color3.fromRGB(255, 255, 255)
 UIStroke2.ApplyStrokeMode = Enum.ApplyStrokeMode.Border
 UIStroke2.Thickness = 2
-UIStroke2.Parent = CNoClip
-
-local DInvencible = Instance.new("TextButton")
-DInvencible.Name = "DInvencible"
-DInvencible.Size = UDim2.new(0, 200, 0, 50)
-DInvencible.BorderColor3 = Color3.fromRGB(255, 255, 255)
-DInvencible.BackgroundTransparency = 0.1
-DInvencible.BorderSizePixel = 0
-DInvencible.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
-DInvencible.FontSize = Enum.FontSize.Size14
-DInvencible.TextStrokeTransparency = 0
-DInvencible.TextSize = 14
-DInvencible.TextColor3 = Color3.fromRGB(255, 255, 255)
-DInvencible.Text = "Invencible"
-DInvencible.TextWrapped = true
-DInvencible.TextWrap = true
-DInvencible.Font = Enum.Font.FredokaOne
-DInvencible.TextScaled = true
-DInvencible.Parent = ScrollingFrame
-
-local UICorner4 = Instance.new("UICorner")
-UICorner4.CornerRadius = UDim.new(0, 12)
-UICorner4.Parent = DInvencible
-
-local UIStroke3 = Instance.new("UIStroke")
-UIStroke3.Color = Color3.fromRGB(255, 255, 255)
-UIStroke3.ApplyStrokeMode = Enum.ApplyStrokeMode.Border
-UIStroke3.Thickness = 2
-UIStroke3.Parent = DInvencible
-
-local AElevation = Instance.new("TextButton")
-AElevation.Name = "AElevation"
-AElevation.Size = UDim2.new(0, 200, 0, 50)
-AElevation.BorderColor3 = Color3.fromRGB(255, 255, 255)
-AElevation.BackgroundTransparency = 0.1
-AElevation.BorderSizePixel = 0
-AElevation.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
-AElevation.FontSize = Enum.FontSize.Size14
-AElevation.TextStrokeTransparency = 0
-AElevation.TextSize = 14
-AElevation.TextColor3 = Color3.fromRGB(255, 255, 255)
-AElevation.Text = "Elevation"
-AElevation.TextWrapped = true
-AElevation.TextWrap = true
-AElevation.Font = Enum.Font.FredokaOne
-AElevation.TextScaled = true
-AElevation.Parent = ScrollingFrame
-
-local UICorner5 = Instance.new("UICorner")
-UICorner5.CornerRadius = UDim.new(0, 12)
-UICorner5.Parent = AElevation
-
-local UIStroke4 = Instance.new("UIStroke")
-UIStroke4.Color = Color3.fromRGB(255, 255, 255)
-UIStroke4.ApplyStrokeMode = Enum.ApplyStrokeMode.Border
-UIStroke4.Thickness = 2
-UIStroke4.Parent = AElevation
-
-local XListBR = Instance.new("TextButton")
-XListBR.Name = "XListBR"
-XListBR.Size = UDim2.new(0, 200, 0, 50)
-XListBR.BorderColor3 = Color3.fromRGB(255, 255, 255)
-XListBR.BackgroundTransparency = 0.1
-XListBR.BorderSizePixel = 0
-XListBR.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
-XListBR.FontSize = Enum.FontSize.Size14
-XListBR.TextStrokeTransparency = 0
-XListBR.TextSize = 14
-XListBR.TextColor3 = Color3.fromRGB(255, 255, 255)
-XListBR.Text = "List BRoots"
-XListBR.TextWrapped = true
-XListBR.TextWrap = true
-XListBR.Font = Enum.Font.FredokaOne
-XListBR.TextScaled = true
-XListBR.Parent = ScrollingFrame
-
-local UICorner6 = Instance.new("UICorner")
-UICorner6.CornerRadius = UDim.new(0, 12)
-UICorner6.Parent = XListBR
-
-local UIStroke5 = Instance.new("UIStroke")
-UIStroke5.Color = Color3.fromRGB(255, 255, 255)
-UIStroke5.ApplyStrokeMode = Enum.ApplyStrokeMode.Border
-UIStroke5.Thickness = 2
-UIStroke5.Parent = XListBR
+UIStroke2.Parent = ESalto
 
 local UIAspectRatioConstraint = Instance.new("UIAspectRatioConstraint")
 UIAspectRatioConstraint.AspectRatio = 1.0215054
 UIAspectRatioConstraint.Parent = ScrollingFrame
 
-local TextLabel = Instance.new("TextLabel") -- UI TITLE
+local DVelocidade = Instance.new("TextButton")
+DVelocidade.Name = "DVelocidade"
+DVelocidade.Size = UDim2.new(0, 200, 0, 50)
+DVelocidade.BorderColor3 = Color3.fromRGB(255, 255, 255)
+DVelocidade.BackgroundTransparency = 0.1
+DVelocidade.BorderSizePixel = 0
+DVelocidade.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
+DVelocidade.FontSize = Enum.FontSize.Size14
+DVelocidade.TextStrokeTransparency = 0
+DVelocidade.TextSize = 14
+DVelocidade.TextColor3 = Color3.fromRGB(255, 255, 255)
+DVelocidade.Text = "Velocidade"
+DVelocidade.TextWrapped = true
+DVelocidade.TextWrap = true
+DVelocidade.Font = Enum.Font.FredokaOne
+DVelocidade.TextScaled = true
+DVelocidade.Parent = ScrollingFrame
+
+local UICorner4 = Instance.new("UICorner")
+UICorner4.CornerRadius = UDim.new(0, 12)
+UICorner4.Parent = DVelocidade
+
+local UIStroke3 = Instance.new("UIStroke")
+UIStroke3.Color = Color3.fromRGB(255, 255, 255)
+UIStroke3.ApplyStrokeMode = Enum.ApplyStrokeMode.Border
+UIStroke3.Thickness = 2
+UIStroke3.Parent = DVelocidade
+
+local CTeleportBase = Instance.new("TextButton")
+CTeleportBase.Name = "CTeleportBase"
+CTeleportBase.Size = UDim2.new(0, 200, 0, 50)
+CTeleportBase.BorderColor3 = Color3.fromRGB(255, 255, 255)
+CTeleportBase.BackgroundTransparency = 0.1
+CTeleportBase.BorderSizePixel = 0
+CTeleportBase.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
+CTeleportBase.FontSize = Enum.FontSize.Size14
+CTeleportBase.TextStrokeTransparency = 0
+CTeleportBase.TextSize = 14
+CTeleportBase.TextColor3 = Color3.fromRGB(255, 255, 255)
+CTeleportBase.Text = "Fast TP"
+CTeleportBase.TextWrapped = true
+CTeleportBase.TextWrap = true
+CTeleportBase.Font = Enum.Font.FredokaOne
+CTeleportBase.TextScaled = true
+CTeleportBase.Parent = ScrollingFrame
+
+local UICorner5 = Instance.new("UICorner")
+UICorner5.CornerRadius = UDim.new(0, 12)
+UICorner5.Parent = CTeleportBase
+
+local UIStroke4 = Instance.new("UIStroke")
+UIStroke4.Color = Color3.fromRGB(255, 255, 255)
+UIStroke4.ApplyStrokeMode = Enum.ApplyStrokeMode.Border
+UIStroke4.Thickness = 2
+UIStroke4.Parent = CTeleportBase
+
+local BElevation = Instance.new("TextButton")
+BElevation.Name = "BElevation"
+BElevation.Size = UDim2.new(0, 200, 0, 50)
+BElevation.BorderColor3 = Color3.fromRGB(255, 255, 255)
+BElevation.BackgroundTransparency = 0.1
+BElevation.BorderSizePixel = 0
+BElevation.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
+BElevation.FontSize = Enum.FontSize.Size14
+BElevation.TextStrokeTransparency = 0
+BElevation.TextSize = 14
+BElevation.TextColor3 = Color3.fromRGB(255, 255, 255)
+BElevation.Text = "Elevation"
+BElevation.TextWrapped = true
+BElevation.TextWrap = true
+BElevation.Font = Enum.Font.FredokaOne
+BElevation.TextScaled = true
+BElevation.Parent = ScrollingFrame
+
+local UICorner6 = Instance.new("UICorner")
+UICorner6.CornerRadius = UDim.new(0, 12)
+UICorner6.Parent = BElevation
+
+local UIStroke5 = Instance.new("UIStroke")
+UIStroke5.Color = Color3.fromRGB(255, 255, 255)
+UIStroke5.ApplyStrokeMode = Enum.ApplyStrokeMode.Border
+UIStroke5.Thickness = 2
+UIStroke5.Parent = BElevation
+
+local TextLabel = Instance.new("TextLabel")
 TextLabel.Size = UDim2.new(0.6842105, 0, 0.1555556, 0)
 TextLabel.BorderColor3 = Color3.fromRGB(0, 0, 0)
 TextLabel.BackgroundTransparency = 1
@@ -281,11 +271,11 @@ UIStroke6.Color = Color3.fromRGB(255, 255, 255)
 UIStroke6.Thickness = 2
 UIStroke6.Parent = Frame
 
-local TextLabel1 = Instance.new("TextLabel") -- VERSION TITLE
+local TextLabel1 = Instance.new("TextLabel")
 TextLabel1.Size = UDim2.new(0.3052632, 0, 0.0638889, 0)
 TextLabel1.BorderColor3 = Color3.fromRGB(0, 0, 0)
 TextLabel1.BackgroundTransparency = 1
-TextLabel1.Position = UDim2.new(0.73, 0, 0.9338543, 0)
+TextLabel1.Position = UDim2.new(0.7095547, 0, 0.9293122, 0)
 TextLabel1.BorderSizePixel = 0
 TextLabel1.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
 TextLabel1.FontSize = Enum.FontSize.Size14
@@ -340,13 +330,12 @@ TextBox.Position = UDim2.new(-0.7789474, 0, 0.0083333, 0)
 TextBox.BorderSizePixel = 0
 TextBox.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
 TextBox.FontSize = Enum.FontSize.Size14
-TextBox.TextSize = 10
+TextBox.TextSize = 14
 TextBox.TextColor3 = Color3.fromRGB(255, 255, 255)
 TextBox.Text = ""
-TextBox.CursorPosition = -1
-TextBox.MultiLine = true
 TextBox.Font = Enum.Font.FredokaOne
 TextBox.ClearTextOnFocus = false
+TextBox.MultiLine = true
 TextBox.Parent = Frame
 
 local UICorner8 = Instance.new("UICorner")
@@ -361,7 +350,32 @@ local UIAspectRatioConstraint5 = Instance.new("UIAspectRatioConstraint")
 UIAspectRatioConstraint5.AspectRatio = 0.7916666
 UIAspectRatioConstraint5.Parent = Frame
 
-local open = Instance.new("TextButton") -- BUTTON TITLE
+local Value = Instance.new("TextBox")
+Value.Name = "Value"
+Value.Size = UDim2.new(0.2576399, 0, 0.1568961, 0)
+Value.BorderColor3 = Color3.fromRGB(0, 0, 0)
+Value.BackgroundTransparency = 0.1
+Value.Position = UDim2.new(1.0561657, 0, 0.29893, 0)
+Value.BorderSizePixel = 0
+Value.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
+Value.FontSize = Enum.FontSize.Size14
+Value.TextWrapped = true
+Value.TextWrap = true
+Value.TextSize = 14
+Value.TextColor3 = Color3.fromRGB(255, 99, 0)
+Value.PlaceholderText = "Value"
+Value.Text = ""
+Value.CursorPosition = -1
+Value.Font = Enum.Font.FredokaOne
+Value.Parent = Frame
+TextBox.ClearTextOnFocus = false
+TextBox.MultiLine = true
+
+local UICorner9 = Instance.new("UICorner")
+UICorner9.CornerRadius = UDim.new(0, 12)
+UICorner9.Parent = Value
+
+local open = Instance.new("TextButton")
 open.Name = "open"
 open.Visible = false
 open.Size = UDim2.new(0.0422544, 0, 0.0739076, 0)
@@ -378,9 +392,9 @@ open.TextWrap = true
 open.Font = Enum.Font.FredokaOne
 open.Parent = ScreenGui
 
-local UICorner9 = Instance.new("UICorner")
-UICorner9.CornerRadius = UDim.new(1, 0)
-UICorner9.Parent = open
+local UICorner10 = Instance.new("UICorner")
+UICorner10.CornerRadius = UDim.new(1, 0)
+UICorner10.Parent = open
 
 local UIStroke7 = Instance.new("UIStroke")
 UIStroke7.Color = Color3.fromRGB(255, 255, 255)
@@ -403,6 +417,14 @@ open.MouseButton1Click:Connect(function()
 	Frame.Visible = true
 	open.Visible = false
 end)
+
+local function getNumericValue()
+	local val = tonumber(Value.Text)
+	if val == nil then
+		return nil
+	end
+	return val
+end
 
 -- Função que verifica se o nome contém uma das palavras da lista
 local function shouldIgnore(name)
@@ -455,27 +477,21 @@ end
 
 
 -- NO CLIP
-local function matchesList(name, list)
-	local lowerName = string.lower(name)
-	for _, word in ipairs(list) do
-		if string.find(lowerName, string.lower(word)) then
-			return true
-		end
-	end
-	return false
+local function teleportForward()
+	-- Atualiza as referências (caso o personagem tenha respawnado)
+	Character = plr.Character or plr.CharacterAdded:Wait()
+	local HumanoidRootPart = Character:FindFirstChild("HumanoidRootPart")
+	if not HumanoidRootPart then return end
+	-- Pega a direção da câmera (onde o player está olhando)
+	local lookVector = Camera.CFrame.LookVector
+	-- Calcula a nova posição
+	local val = getNumericValue()
+	if not val then return end
+	local newPosition = HumanoidRootPart.Position + (lookVector * val)
+	-- Move o player pra lá
+	HumanoidRootPart.CFrame = CFrame.new(newPosition, newPosition + lookVector)
 end
 
-local function applyNoClip()
-	for _, obj in ipairs(workspace:GetDescendants()) do
-		if obj:IsA("BasePart") and matchesList(obj.Name, NoClipList) then
-			local parentModel = obj:FindFirstAncestorOfClass("Model")
-			local parentfolder = obj:FindFirstAncestorOfClass("Folder")
-			if parentModel and matchesList(parentModel.Name, NoClipModelsList) or parentfolder and matchesList(parentfolder.Name, NoClipModelsList) then
-				obj.CanCollide = false
-			end
-		end
-	end
-end
 
 -- Buttons
 
@@ -490,7 +506,27 @@ ZListins.MouseButton1Click:Connect(function()
 	end
 end)
 
-CNoClip.MouseButton1Click:Connect(function()
-		CNoClip.BackgroundColor3 = Color3.new(0,255,0)
-		applyNoClip()
+ADash.MouseButton1Click:Connect(function()
+		teleportForward()
+end)
+
+DVelocidade.MouseButton1Click:Connect(function()
+	Character = plr.Character or plr.CharacterAdded:Wait()
+	local Humanoid = Character:WaitForChild("Humanoid")
+	local val = getNumericValue()
+	if not val then return end
+	Humanoid.WalkSpeed = val
+end)
+
+-- Altera altura do pulo
+ESalto.MouseButton1Click:Connect(function()
+	Character = plr.Character or plr.CharacterAdded:Wait()
+	local Humanoid = Character:WaitForChild("Humanoid")
+	local val = getNumericValue()
+	if not val then return end
+	if Humanoid.JumpHeight then
+		Humanoid.JumpHeight = val
+	else
+		Humanoid.JumpPower = val
+	end
 end)
